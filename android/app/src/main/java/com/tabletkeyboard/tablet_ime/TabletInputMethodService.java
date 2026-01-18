8464940beed1ea92117013cbbc0a074286d46b46
SHAOQIAN LIANG <phaibo@aliyun.com>
2026-01-18 14:42:37 +0800

    Add files via upload

--- android/app/src/main/java/com/tabletkeyboard/tablet_ime/TabletInputMethodService.java
+++ android/app/src/main/java/com/tabletkeyboard/tablet_ime/TabletInputMethodService.java
@@ -1,18 +1,13 @@
 package com.tabletkeyboard.tablet_ime;
 
 import android.inputmethodservice.InputMethodService;
-import android.inputmethodservice.Keyboard;
-import android.inputmethodservice.KeyboardView;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
-import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugin.common.MethodChannel;
 
 public class TabletInputMethodService extends InputMethodService {
-    private static final String CHANNEL = "com.tabletkeyboard/tablet_ime";
-    private MethodChannel methodChannel;
+
     private InputConnection inputConnection;
 
     @Override
@@ -22,56 +17,13 @@ public class TabletInputMethodService extends InputMethodService {
 
     @Override
     public View onCreateInputView() {
-        FlutterActivity activity = new FlutterActivity() {
-            @Override
-            protected void configureFlutterEngine(FlutterEngine flutterEngine) {
-                super.configureFlutterEngine(flutterEngine);
-                methodChannel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL);
-                methodChannel.setMethodCallHandler((call, result) -> {
-                    switch (call.method) {
-                        case "sendText":
-                            String text = call.argument("text");
-                            if (inputConnection != null) {
-                                inputConnection.commitText(text, 1);
-                            }
-                            result.success(null);
-                            break;
-                        case "deleteText":
-                            if (inputConnection != null) {
-                                inputConnection.deleteSurroundingText(1, 0);
-                            }
-                            result.success(null);
-                            break;
-                        case "sendKeyEvent":
-                            int keyCode = call.argument("keyCode");
-                            boolean isDown = call.argument("isDown");
-                            if (inputConnection != null) {
-                                inputConnection.sendKeyEvent(new KeyEvent(
-                                    isDown ? KeyEvent.ACTION_DOWN : KeyEvent.ACTION_UP,
-                                    keyCode
-                                ));
-                            }
-                            result.success(null);
-                            break;
-                        case "commitText":
-                            String commitText = call.argument("text");
-                            if (inputConnection != null) {
-                                inputConnection.commitText(commitText, 1);
-                            }
-                            result.success(null);
-                            break;
-                        default:
-                            result.notImplemented();
-                    }
-                });
-            }
-        };
-
-        return activity.onCreateView(null);
+        // Return a simple view for the IME
+        View view = new View(this);
+        return view;
     }
 
     @Override
-    public void onStartInput(View attribute, boolean restarting) {
+    public void onStartInput(EditorInfo attribute, boolean restarting) {
         super.onStartInput(attribute, restarting);
         inputConnection = getCurrentInputConnection();
     }
@@ -80,5 +32,32 @@ public class TabletInputMethodService extends InputMethodService {
     public void onStartInputView(EditorInfo info, boolean restarting) {
         super.onStartInputView(info, restarting);
         inputConnection = getCurrentInputConnection();
+    }
+
+    public void sendText(String text) {
+        if (inputConnection != null) {
+            inputConnection.commitText(text, 1);
+        }
+    }
+
+    public void deleteText() {
+        if (inputConnection != null) {
+            inputConnection.deleteSurroundingText(1, 0);
+        }
+    }
+
+    public void sendKeyEvent(int keyCode, boolean isDown) {
+        if (inputConnection != null) {
+            inputConnection.sendKeyEvent(new KeyEvent(
+                isDown ? KeyEvent.ACTION_DOWN : KeyEvent.ACTION_UP,
+                keyCode
+            ));
+        }
+    }
+
+    public void commitText(String text) {
+        if (inputConnection != null) {
+            inputConnection.commitText(text, 1);
+        }
     }
 }
